25833be0ed7d5257f8ade20ca0bc6f8258c26192
m4noj <m4noj98@gmail.com>
2018-12-23 10:30:33 +0530

    added post edit link

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,7 +23,7 @@
 } ?>	
 	<li><a href='admin'>Admin</a></li>			
 <?php 
-	if(isset($_SESSION['user_role'])){
+	if(isset($_SESSION['username'])){
 		if(isset($_GET['p_id'])){
 			$post_id = $_GET['p_id'];
 			echo "<li><a href='admin/posts.php?source=edit_post&p_id={$post_id}'>Edit Post</a></li>";
