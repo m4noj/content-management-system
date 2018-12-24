be88a01fc0bb1cc28cdb5eee6553c620c0229fcf
m4noj <m4noj98@gmail.com>
2018-12-24 20:58:39 +0530

    added alignment feature plugin

--- admin/js/ckeditor/main.js
+++ admin/js/ckeditor/main.js
@@ -1,5 +1,12 @@
 ClassicEditor
-    .create(document.querySelector('#editor'))
-	.catch(error => {
-        console.error(error);
-    });
+	.create(document.querySelector('#editor'), {
+		toolbar: [
+		'heading', '|', 'bold', 'italic', 'blockQuote', 'bulletedList', 'numberedList', 'link', 'alignment:left', 'alignment:right', 'alignment:center', 'alignment:justify', 'imageUpload', 'mediaEmbed', 'insertTable', 'undo', 'redo'
+	]
+	})
+	.then(editor => {
+		window.editor = editor;
+	})
+	.catch(err => {
+		console.log(err.stack);
+	})
