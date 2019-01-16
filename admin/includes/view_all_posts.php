07a7fd6f1d9d9816f5a6db2966fab5a6c6d3b7cf
m4noj <m4noj98@gmail.com>
2019-01-16 20:24:40 +0530

    added bulk options and links to post titles

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,6 +1,44 @@
+<?php 
+if(isset($_POST['checkArray'])){
+  foreach($_POST['checkArray'] as $checkId){
+	$bulk_opt = $_POST['bulk-opt'];
+	switch($bulk_opt){
+		case 'published':
+			$publish_query = "UPDATE posts SET post_status = '$bulk_opt' WHERE post_id = $checkId ";
+			$publish_query_result = mysqli_query($connection,$publish_query);
+		break;
+		case 'draft':
+			$draft_query = "UPDATE posts SET post_status = '$bulk_opt' WHERE post_id = $checkId ";
+			$draft_query_result = mysqli_query($connection,$draft_query);
+			break;
+		case 'delete':
+			$del_query = "DELETE FROM posts WHERE post_id = $checkId ";
+			$del_query_result = mysqli_query($connection,$del_query);
+			break;
+		default:
+			echo "<b>Please select an Option</b></br></br>";
+		break;
+		}
+	}
+}
+?>
+<form action="" method="post">
 <table class="table table-hover table-bordered">
+	<div id="bulk-opt-container" class="col-xs-4" >
+		<select class="form-control" name="bulk-opt">
+			<option value="" disabled selected>Select Option</option>
+			<option value="published">Publish</option>
+			<option value="draft">Draft</option>
+			<option value="delete">Delete</option>
+		</select>
+	</div>
+	<div class="col-xs-4">
+		<input type="submit" name="submit" value="Apply" class="btn btn-success">
+		<a href="posts.php?source=add_post" class="btn btn-primary">Add New</a>
+	</div><br><br><br>
 	<thead>
 		<tr>
+			<th class="text-center"><input type='checkbox' id='bulkCheck'></th>
 			<th class="text-center">Id</th>
 			<th class="text-center">Title</th>
 			<th class="text-center">Author</th>
@@ -30,8 +68,9 @@
 			$post_date = $row['post_date'];
 		echo "
 			<tr>
+				<td class='text-center'><input type='checkbox' name='checkArray[]' class='checkBoxes' value='$post_id'></td>
 				<td class='text-center'>$post_id</td>
-				<td class='text-center'>$post_title</td>
+				<td class='text-center'><a href='../post.php?p_id=$post_id'>$post_title</a></td>
 				<td class='text-center'>$post_author</td>";
 				$query = "SELECT * FROM categories WHERE cat_id = '$post_cat_id' ";			
 				$select_cat = mysqli_query($connection,$query);
@@ -50,6 +89,7 @@
 		} ?>
 	</tbody>
 </table>
+</form>
 
 <?php
 	// delete post
@@ -58,7 +98,5 @@ if(isset($_GET['delete'])){
 	$post_del_query = "DELETE FROM posts WHERE post_id = $post_del_id ";
 	$del_query_result = mysqli_query($connection,$post_del_query);
 	header("Location: posts.php");
-}
-
+ }
 ?>
-
