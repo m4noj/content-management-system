9f3273ebbcf628c9e6640028161c32a780fea685
m4noj <m4noj98@gmail.com>
2019-01-16 20:42:11 +0530

    feature:dynamic personalization to Admin

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -13,10 +13,10 @@
 <ul class="nav navbar-right top-nav">
 	<li><a href="../index.php">Home</a></li>
 	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Manoj <b class="caret"></b></a>
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username']; ?> <b class="caret"></b></a>
 		<ul class="dropdown-menu">
 			<li>
-				<a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
+				<a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>	
 			</li>
 			<li class="divider"></li> 
 			<li>
