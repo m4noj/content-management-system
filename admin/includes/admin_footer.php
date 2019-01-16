b5c7aadc7623ef18f95fdc7d477fa639f01bf728
m4noj <m4noj98@gmail.com>
2019-01-16 20:31:57 +0530

    added link to script

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -4,6 +4,9 @@
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
 
+<!-- custom script -->
+<script src="js/main.js"></script>
+
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
