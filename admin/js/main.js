2747d78519ee5bd41c4d7716096549eb9a864231
m4noj <m4noj98@gmail.com>
2019-03-20 18:18:39 +0530

    added: loader animation

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-$('#bulkCheck').click(function() {
-	if (this.checked) {
-		$('.checkBoxes').each(function() {
-			this.checked = true;
-		});
-	} else {
-		$('.checkBoxes').each(function() {
-			this.checked = false;
-		});
-	}
-  });
+	$('#bulkCheck').click(function() {
+		if (this.checked) {
+			$('.checkBoxes').each(function() {
+				this.checked = true;
+			});
+		} else {
+			$('.checkBoxes').each(function() {
+				this.checked = false;
+			});
+		}
+	});
+	
+	let div_box = "<div id='load-screen'><div id='loading'></div></div>";
+	$("body").prepend(div_box);
+	$('#load-screen').delay(500).fadeOut(600, function() {
+		$(this).remove();
+	});
 });
