61d7da400924ec56d9928c668a06aa7d177a3860
m4noj <m4noj98@gmail.com>
2019-03-23 19:09:14 +0530

    added pagination feature

--- index.php
+++ index.php
@@ -12,23 +12,39 @@
 <div class="col-md-8">
 <h1 class="page-header">
 	All Recent Posts
-	<small>Secondary Text</small>
+<small>Secondary Text</small>
 </h1>
-
+	
 <?php 
-$query = "SELECT * FROM posts WHERE post_status = 'published' ORDER BY post_id DESC";
-$posts_row = mysqli_query($connection,$query);
-while($row = mysqli_fetch_assoc($posts_row)){
-	$post_id = $row['post_id'];
-	$post_title = $row['post_title'];
-	$post_author = $row['post_author'];
-	$post_date = $row['post_date'];
-	$post_img = $row['post_img'];
-	$post_status = $row['post_status'];
-	$post_content = substr($row['post_content'],0,150); 
-	if($post_status == 'published'){
-?>
+	$per_page = 5;
+	if(isset($_GET['page'])){
+		$page = $_GET['page'];
+	} else {
+		$page = '';
+	}
+	if($page == '' || $page == 1){
+		$page_1 = 0;
+	} else {
+		$page_1 = ($page * $per_page) - $per_page;
+	}
 
+	$posts_count_query = "SELECT * FROM posts";
+	$res_posts_count = mysqli_query($connection,$posts_count_query);
+	$post_counts = ceil(mysqli_num_rows($res_posts_count)/5);
+	
+	$query = "SELECT * FROM posts WHERE post_status = 'published' ORDER BY post_id DESC LIMIT $page_1, $per_page";
+	$posts_row = mysqli_query($connection,$query);
+		while($row = mysqli_fetch_assoc($posts_row)){
+			$post_id = $row['post_id'];
+			$post_title = $row['post_title'];
+			$post_author = $row['post_author'];
+			$post_date = $row['post_date'];
+			$post_img = $row['post_img'];
+			$post_status = $row['post_status'];
+			$post_content = substr($row['post_content'],0,150); 
+	
+		if($post_status == 'published'){
+?>
 <h2>
 	<a href="post.php?p_id=<?php echo $post_id; ?>">
 		<?php echo $post_title; ?></a>
@@ -42,7 +58,7 @@ while($row = mysqli_fetch_assoc($posts_row)){
 </p>
 <hr>
 <a href="post.php?p_id=<?php echo $post_id; ?>">
-<img class="img-responsive img-900x300" src="img/<?php echo $post_img;?>" alt=""></a>
+<img class="img-responsive img-900x300" src="images/<?php echo $post_img;?>" alt=""></a>
 <hr>
 <p>
 	<?php echo $post_content; ?>
@@ -59,8 +75,20 @@ while($row = mysqli_fetch_assoc($posts_row)){
 
 </div>
 <!-- /.row -->
-
+<br>
 <hr>
+<!-- pagination -->
+<ul class="pager">
+<?php 
+	for($i = 1; $i <= $post_counts; $i++){
+		if($i == $page){
+			echo "<li><a href='index.php?page=$i' class='active-link'>$i</a></li>";    
+		} else {
+			echo "<li><a href='index.php?page=$i'>$i</a></li>";    
+		}
+	}
+?>	
+</ul>
 
 <!-- Footer -->
 <?php include "includes/footer.php" ?>
